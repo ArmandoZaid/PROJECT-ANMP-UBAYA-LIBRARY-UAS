1ce2481b59b6ac5a6dd6ac003427b97156838a05
nmuflih12 <n.muflih12@gmail.com>
2022-06-30 22:04:02 +0700

    UPDATE MUFLIH

--- app/src/main/java/com/ubaya/uas_project_ubaya_library/view/createBookFragment.kt
+++ app/src/main/java/com/ubaya/uas_project_ubaya_library/view/createBookFragment.kt
@@ -1,18 +1,21 @@
 package com.ubaya.uas_project_ubaya_library.view
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.Navigation
 import com.ubaya.uas_project_ubaya_library.R
 import com.ubaya.uas_project_ubaya_library.databinding.FragmentCreateBookBinding
 import com.ubaya.uas_project_ubaya_library.model.Library
 import com.ubaya.uas_project_ubaya_library.viewModel.BookListViewModel
 import com.ubaya.uas_project_ubaya_library.viewModel.DetailBookViewModel
+import kotlinx.android.synthetic.main.fragment_create_book.view.*
 
 
 class createBookFragment : Fragment(), CreateBookListener {
@@ -37,16 +40,19 @@ class createBookFragment : Fragment(), CreateBookListener {
     }
 
     private fun observiewModel() {
-        viewModel.bookLD.observe(viewLifecycleOwner) {
-            dataBinding.book = it
-        }
+        dataBinding.book = Library( "","","",0)
+        viewModel = ViewModelProvider(this).get(DetailBookViewModel::class.java)
     }
 
     override fun onCreateBookClick(
         view: View,
-        obj: List<Library>,
+        obj: Library,
     ){
-        viewModel.addTodo(obj)
+        var list = listOf(obj)
+        var book = listOf(Library( dataBinding.createTitle.text.toString(), dataBinding.createDescription.text.toString(),
+            dataBinding.createAuthor.text.toString(), dataBinding.createRating.text.toString().toInt()))
+        Log.d("sedfdsfd", dataBinding.createTitle.text.toString());
+        viewModel.addTodo(book)
         Toast.makeText(view.context, "Todo updated", Toast.LENGTH_SHORT).show()
         Navigation.findNavController(view).popBackStack()
     }
