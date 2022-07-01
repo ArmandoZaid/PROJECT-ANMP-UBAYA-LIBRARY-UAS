fc3887b021171376455fdbf8ce0587c3db742334
nmuflih12 <n.muflih12@gmail.com>
2022-07-01 08:13:40 +0700

    FINAL COMMIT (DELETE BELUM BISA)

--- app/src/main/java/com/ubaya/uas_project_ubaya_library/view/AboutCreatorFragment.kt
+++ app/src/main/java/com/ubaya/uas_project_ubaya_library/view/AboutCreatorFragment.kt
@@ -5,13 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.Navigation
 import com.ubaya.uas_project_ubaya_library.R
-import kotlinx.android.synthetic.main.fragment_about.*
 
 
-class AboutFragment : Fragment() {
-
+class AboutCreatorFragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
@@ -21,14 +18,8 @@ class AboutFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_about, container, false)
+        return inflater.inflate(R.layout.fragment_about_creator, container, false)
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        buttonAbtCreator.setOnClickListener{
-            val action = AboutFragmentDirections.actionAboutCreator()
-            Navigation.findNavController(it).navigate(action)
-        }
-    }
 
 }
