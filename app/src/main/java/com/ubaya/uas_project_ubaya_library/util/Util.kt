b4c14c11235cd64c81fef9776c84407beaeb713a
nmuflih12 <n.muflih12@gmail.com>
2022-07-01 09:06:58 +0700

    FINAL COMMIT v3 perbaikan migration (DELETE BELUM BISA)

--- app/src/main/java/com/ubaya/uas_project_ubaya_library/util/Util.kt
+++ app/src/main/java/com/ubaya/uas_project_ubaya_library/util/Util.kt
@@ -22,7 +22,7 @@ fun buildDb(context: Context) = Room.databaseBuilder(context,
 
 val MIGRATION_1_2 = object : Migration(1, 2) {
     override fun migrate(database: SupportSQLiteDatabase) {
-        database.execSQL(" ALTER TABLE todo ADD COLUMN priority INTEGER DEFAULT 3 NOT NULL")
+        database.execSQL(" ALTER TABLE newlibrarydb ADD COLUMN priority INTEGER DEFAULT 3 NOT NULL")
     }
 }
 
